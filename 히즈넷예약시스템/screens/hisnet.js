05536f28c12bfba3a4adc01222a6bcfadb1efea8
mjaepark <114917646+mjaepark@users.noreply.github.com>
2022-10-15 19:55:17 +0900

    Add files via upload

---
+++ 히즈넷예약시스템/screens/hisnet.js
@@ -0,0 +1,13 @@
+// var cancel = document.getElementById('cancel');
+function confirmcancel(){
+    // var confirmflag = confirm("취소하시겠습니까?");
+    if(confirmflag==true){
+        document.write("예약이 취소되었습니다")
+    }
+    else if(confirmflag==false){
+        document.write("뒤로")
+    }
+}
+// cancel.addEventListener('click',function(){
+//     alert("취소되었습니다");
+// })
